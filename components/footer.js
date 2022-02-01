bd0caf735679b12ce974e75c7137c50a2d340c16
fredriklexo <91253151+fredriklexo@users.noreply.github.com>
2022-02-01 15:45:26 +0100

    Fixing frontend for product page

---
+++ components/footer.js
@@ -0,0 +1,23 @@
+document.getElementById("footer").innerHTML = `
+
+<!-- Brand Category Card  -->
+
+<div class="category-container">
+    <div class="swiperTwo s2 ">
+      <div class="swiper-wrapperTwo">
+        <div class="swiper-slideTwo">Slide 1</div>
+        <div class="swiper-slideTwo">Slide 2</div>
+        <div class="swiper-slideTwo">Slide 3</div>
+        <div class="swiper-slideTwo">Slide 4</div>
+        <div class="swiper-slideTwo">Slide 5</div>
+        <div class="swiper-slideTwo">Slide 6</div>
+        <div class="swiper-slideTwo">Slide 7</div>
+        <div class="swiper-slideTwo">Slide 8</div>
+        <div class="swiper-slideTwo">Slide 9</div>
+      </div>
+      <div class="swiper-paginationTwo"></div>
+    </div>
+</div>
+
+
+`
---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sneakers</title>
+    <!-- CSS -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/common.css">
+    <link rel="stylesheet" href="./css/mediaQueries.css">
+    <link rel="stylesheet" href="./css/navbar.css">
+    <link rel="stylesheet" href="./css/productCard.css">
+
+    <!-- JS common -->
+    <script defer src="./components/productCard.js"></script>
+    <script defer src="./components/navbar.js"></script>
+    <script defer src="./components/footer.js"></script>
+
+
+    <!-- SWIPER -->
+   <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css"/> 
+   <script defer src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+    <script defer src="swiper.js" type="text/javascript"></script> 
+
+</head>
+<body>
+    <!-- navbar -->
+    <div id="navbar" class="zindexThree"></div>
+
+    <!-- background for productCard -->
+    <div class="background"></div>
+
+    <!-- productCard -->
+    <div id="productCard" class="zindexOne"></div>
+    <!-- productCard info when use MQ -->
+    <div id="productCardInfo"></div>
+
+    <!-- footer -->
+    <div id="footer" class="zindexOne"></div>
+   
+</body>
+</html>
+
