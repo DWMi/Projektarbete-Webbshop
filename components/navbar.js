2edbfbb77736d9d7a877896b9b57a8cc2271d46d
antoniowav <85946713+antoniowav@users.noreply.github.com>
2022-02-07 18:12:21 +0100

    added login and sign in pages, made some changes to navbar

--- components/navbar.js
+++ components/navbar.js
@@ -3,7 +3,7 @@ document.getElementById("navbar").innerHTML = `
 
 <div class="navBar-container">
 
-    <div class="navBar-logo"><a href="./landing.html"><h1>SNEAKERS<span>.</span></h1></a></div>
+    <div class="navBar-logo"><a href="./login.html"><h1>SNEAKERS<span>.</span></h1></a></div>
 
     <div class="navBar-category ">
         <div class="navbtn dropdown">
@@ -12,10 +12,11 @@ document.getElementById("navbar").innerHTML = `
             </button></div>
             
             <div class="dropdown-content">
-                <div class="dropdown-brand"><img src="https://logos-world.net/wp-content/uploads/2020/04/Nike-Logo-1978-present.jpg" alt=""></div>
-                <div class="dropdown-brand"><img src="https://brandfinder.org/sv/wp-content/uploads/Axel-Arigato-logo.png" alt=""></div>
-                <div class="dropdown-brand"><img src="https://w7.pngwing.com/pngs/261/578/png-transparent-adidas-logo-adidas-originals-adidas-stan-smith-adidas-superstar-adidas-text-logo-sneakers.png" alt=""></div>
-                <div class="dropdown-brand"><img src="https://brandfinder.org/sv/wp-content/uploads/Axel-Arigato-logo.png" alt=""></div>
+                <div class="dropdown-brand"><img src="./ASSETS/1.LOGOS/ADIDAS.png" alt=""></div>
+                <div class="dropdown-brand"><img src="./ASSETS/1.LOGOS/CONVERSE.png" alt=""></div>
+                <div class="dropdown-brand"><img src="./ASSETS/1.LOGOS/NEW BALANCE.png" alt=""></div>
+                <div class="dropdown-brand"><img src="./ASSETS/1.LOGOS/NIKE.png" alt=""></div>
+                <div class="dropdown-brand"><img src="./ASSETS/1.LOGOS/SAUCONY.png" alt=""></div>
             </div>
         </div> 
         <div class="navbtn"><h5>ABOUT</h5></div>
