7313912bdc46178d83713e42d955b09f5066c632
antoniowav <85946713+antoniowav@users.noreply.github.com>
2022-02-14 10:24:36 +0100

    sign up and login component

--- components/navbar.js
+++ components/navbar.js
@@ -3,7 +3,7 @@ document.getElementById("navbar").innerHTML = `
 
 <div class="navBar-container">
 
-    <div class="navBar-logo"><a href="./login.html"><h1>SNEAKERS<span>.</span></h1></a></div>
+    <div class="navBar-logo"><a href="#"><h1>SNEAKERS<span>.</span></h1></a></div>
 
     <div class="navBar-category ">
         <div class="navbtn dropdown">
