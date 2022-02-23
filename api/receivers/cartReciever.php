4f2bc3489d59ae58f8845c09cf1761299a69a8c6
Hugo Bengtsson <hugo@capellium.se>
2022-02-23 11:45:43 +0100

    Functionality to add items to cart with session.

---
+++ api/receivers/cartReciever.php
@@ -0,0 +1,54 @@
+<?php
+
+    // lägg till empty cart (POST/DELETE).
+
+    try {
+        session_start();
+        include_once("../controllers/cartController.php");
+
+        if($_SERVER["REQUEST_METHOD"] == "GET") {
+
+            if($_GET["action"] == "getCart") {
+
+                $cartController = new CartController();
+                echo json_encode($cartController->cartReturn());
+
+            }
+
+        }
+
+        if($_SERVER["REQUEST_METHOD"] == "POST") {
+
+            if($_GET["action"] == "addCart") {
+
+                $body = json_decode($_POST["cart"], true);
+
+                $cartController = new CartController();
+                echo json_encode($cartController->addCart($body));
+
+
+            }
+
+
+
+        }
+
+
+
+
+
+
+
+    } catch(Exception $e) {
+        error_log($e);
+    }
+
+
+
+
+
+
+
+
+
+?>
