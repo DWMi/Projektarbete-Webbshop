53d77c575c2fa9e952d94cbcaa2d705d1c59a5a5
David w <cf.davidwong@gmail.com>
2022-02-25 16:43:23 +0100

    fixed index and buttons
    fixed buggy index and added nav user drop down buttons

--- js/index.js
+++ js/index.js
@@ -2,77 +2,51 @@ import  {getAllCategories} from './main.js'
 import {navbar} from '../components/navbar.js'
 
 
-function initSite(){
-
+const initSite=()=>{
     sectionRenderer();
     navbar;
 }
 
 
 
-
-
-
-
-
 // Renders section to landing page
+ const  sectionRenderer = async ()=>  {
 
-async function sectionRenderer() {
-    //Hard coded classes, text links
-    const snapper = ["snapper1", "snapper2", "snapper3", "snapper4", "snapper5"],
-        snapIMG = ["imgSnap1", "imgSnap2", "imgSnap3", "imgSnap4", "imgSnap5"],
-        divContainer = document.getElementById("container"),
-        sectionTagName = document.getElementsByTagName("section"),
-        textLinksList = [
-            "Check out our shoes by adidas",
-            "Check out our shoes by Converse",
-            "Check out our shoes by New Balance",
-            "Check out our shoes by Nike",
-            "Check out our shoes by Saucony"
-        ]
+    const catList = await getAllCategories(),
+    divCon = document.getElementById('container'),
+    footer = document.getElementById('lastFooter')
+    
 
-        
-        
 
-        
 
-        // Loops to create elements
+    for (let i = 0; i < catList.length ; i++){
         
-    for (let i = 0; i < snapper.length; i++) {
-        divContainer.appendChild(document.createElement("section")).setAttribute("class", snapper[i])
-        sectionTagName[i].appendChild(document.createElement("div")).setAttribute("class", snapIMG[i])
-        sectionTagName[i].appendChild(document.createElement("h1")).innerText = `${textLinksList[i]}`
-        
+            const sectSnap = document.createElement('section')
+                divCon.appendChild(sectSnap)
+                sectSnap.setAttribute('class','snapper')
+                sectSnap.appendChild(document.createElement('div')).setAttribute('class','imgSnap')
+                sectSnap.appendChild(document.createElement('h1')).innerText = `Checkout our shoes by ${catList[i].CategoryName}`    
+               
+            //    fetching catID and giving href to brands
+                document.getElementsByTagName('h1')[catList[i].CategoryId].addEventListener("click",function(){
+                            window.location.href = `./product.html?category=${catList[i].CategoryId}`
+                            })
     }
 
         
+        // array to attach attributes to newly created image tags
         
-// array to attach attributes to newly created image tags
-    const categoryList = await getAllCategories();
-    categoryList.map((data, index) => {
-        document.getElementsByClassName(snapIMG[index])[0].style.backgroundImage = `url('/ASSETS/Backgrounds/${data.CategoryName}`+ `.jpg')` 
-
-
-    })
-
-    addCategoryLinks()
-
-    
-}
+        catList.map((data, i) => {
+            document.getElementsByClassName('imgSnap')[i].style.backgroundImage = `url('/ASSETS/Backgrounds/${data.CategoryName}`+ `.jpg')` 
+        })
+        
+        // divCon.appendChild(footer)
+        const incest = document.createElement('footer')
+        incest.appendChild(footer)
+        divCon.appendChild(incest)
  
-
-
-async function addCategoryLinks() {
-    const categoryList = await getAllCategories();
-
-    categoryList.forEach(data => {
-        document.getElementsByTagName('h1')[data.CategoryId].addEventListener("click",function(){
-            window.location.href = `./product.html?category=${data.CategoryId}`
-    })
-})
-
-
+  
 }
 
- 
+
 window.addEventListener("load", initSite);
