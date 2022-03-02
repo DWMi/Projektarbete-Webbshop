7042ebfde2e144122733aa4d81b19409dfe1c3c4
David w <cf.davidwong@gmail.com>
2022-03-02 11:14:09 +0100

    fixed index responsive, checkout button,
    fixed denied access to checkout if not logged in, fixed the dropdown buttons to more" secure", half fixed navbar hamburger. #58

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ import  {getAllCategories} from './main.js'
 import {navbar} from '../components/navbar.js'
 
 
+
 const initSite=()=>{
     sectionRenderer();
     navbar;
@@ -10,7 +11,7 @@ const initSite=()=>{
 
 
 // Renders section to landing page
-const  sectionRenderer = async ()=>  {
+ const  sectionRenderer = async ()=>  {
 
     const catList = await getAllCategories(),
     divCon = document.getElementById('container'),
@@ -21,15 +22,21 @@ const  sectionRenderer = async ()=>  {
 
     for (let i = 0; i < catList.length ; i++){
         
-            const sectSnap = document.createElement('section')
+            const sectSnap = document.createElement('section'),
+                imgCon = document.createElement('div'),
+                imgSnap = document.createElement('img')
                 divCon.appendChild(sectSnap)
+                sectSnap.appendChild(imgCon)
+                imgCon.appendChild(imgSnap)
                 sectSnap.setAttribute('class','snapper')
-                sectSnap.appendChild(document.createElement('div')).setAttribute('class','imgSnap')
+                imgCon.setAttribute('class','imageContainer')
+                imgSnap.setAttribute('class','imgSnap')
+                // sectSnap.appendChild(document.createElement('div')).setAttribute('class','imgSnap')
                 sectSnap.appendChild(document.createElement('h1')).innerText = `Checkout our shoes by ${catList[i].CategoryName}`    
-
+               
             //    fetching catID and giving href to brands
                 document.getElementsByTagName('h1')[catList[i].ID].addEventListener("click",function(){
-                            window.location.href = `./product.html?category=${catList[i].ID}`
+                            window.location.href = `./product.html?category=${catList[i].Id}`
                             })
     }
 
@@ -37,27 +44,15 @@ const  sectionRenderer = async ()=>  {
         // array to attach attributes to newly created image tags
         
         catList.map((data, i) => {
-            document.getElementsByClassName('imgSnap')[i].style.backgroundImage = `url('/ASSETS/Backgrounds/${data.CategoryName}`+ `.jpg')` 
+            document.getElementsByClassName('imgSnap')[i].setAttribute('src',`/ASSETS/Backgrounds/${data.CategoryName}`+ `.jpg`) 
         })
         
         // divCon.appendChild(footer)
         const incest = document.createElement('footer')
         incest.appendChild(footer)
         divCon.appendChild(incest)
-
-
-
-async function addCategoryLinks() {
-    const categoryList = await getAllCategories();
-
-    categoryList.forEach(data => {
-        document.getElementsByTagName('h1')[data.ID].addEventListener("click",function(){
-            window.location.href = `./product.html?category=${data.ID}`
-    })
-})
-
-
-}
+ 
+  
 }
 
 
